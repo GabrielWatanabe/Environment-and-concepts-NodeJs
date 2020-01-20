3bfd1e1f9ec2df190a604db876c88bf58e16efb1
Gabriel Watanabe <gabriel.yujip@gmail.com>
2020-01-20 08:57:09 -0300

    Foi apagado código comentado

--- index.js
+++ index.js
@@ -11,64 +11,6 @@ const project = [
   }
 ];
 
-// server.use((req, res, next) => {
-//   console.log(`Método: ${req.method}; URL: ${req.url}`);
-
-//   next();
-
-//   console.log("finish")
-// })
-
-// function checkUserExists(req, res, next)
-//  {
-//    if (!req.body.name) {
-//      return res.status(400).json({error: 'User name is required'})
-//    }
-
-//    return next()
-//  }
-
-//  function checkUserInArray(req, res, next) {
-//    const user = users[req.params.index];
-
-//    if (!user) {
-//      return res.status(400).json({error: 'User does not exists'});
-//    }
-//    req.user = user;
-//    return next();
-//  }
-// //request body = { "name": "Gabriel"}
-// server.get('/users',checkUserInArray, (req, res) => {
-//   return res.json(users)
-// });
-
-// server.get('/users/:index', checkUserInArray,(req, res) => {
-//   return res.json(req.user)
-// });
-
-// server.post('/users', checkUserExists, (req, res ) => {
-//   const { name } = req.body;
-
-//   users.push(name);
-
-//   return res.json(users);
-// })
-
-// server.put('/users/:index',checkUserInArray, checkUserExists, (req, res) => {
-//   const {index} = req.params;
-//   const {name} = req.body;
-
-//   users[index] = name;
-
-//   return res.json(users)
-// })
-
-// server.delete('/users/:index',checkUserInArray, (req, res) => {
-//   const {index} = req.params;
-//   users.splice(index, 1);
-
-//   return res.json(users)
-// })
 function checkInArray(req, res, next) {
   const eachProject = project[req.params.index];
 
